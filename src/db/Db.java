ce2b35f30c2793bdf51296aadbe34052ea0954d6
mbednarczyk <mbednarczyk@live.com>
2015-06-13 16:28:03 +0200

    Images names changed, added images queries

--- src/db/Db.java
+++ src/db/Db.java
@@ -50,6 +50,36 @@ public class Db implements Serializable {
         questionList.add(new Question("pies", "content/pies.jpg", 1));
         questionList.add(new Question("dom", "content/dom.jpg", 2));
         questionList.add(new Question("java", "content/java.jpg", 3));
+        questionList.add(new Question("ronaldo", "content/1.jpg", 3));
+        questionList.add(new Question("most", "content/2.jpg", 3));
+        questionList.add(new Question("stunt", "content/3.jpg", 3));
+        questionList.add(new Question("pistolet", "content/4.jpg", 1));
+        questionList.add(new Question("zima", "content/5.jpg", 2));
+        questionList.add(new Question("motorowka", "content/6.jpg", 1));
+        questionList.add(new Question("telefony", "content/7.jpg", 2));
+        questionList.add(new Question("windows", "content/8.jpg", 1));
+        questionList.add(new Question("boisko", "content/9.jpg", 1));
+        questionList.add(new Question("motocykl", "content/10.jpg", 1));
+        questionList.add(new Question("samolocik", "content/11.jpg", 2));
+        questionList.add(new Question("lod", "content/12.jpg", 1));
+        questionList.add(new Question("google", "content/13.jpg", 3));
+        questionList.add(new Question("piasek", "content/14.jpg", 1));
+        questionList.add(new Question("malpa", "content/15.jpg", 1));
+        questionList.add(new Question("fajerwerki", "content/16.jpg", 1));
+        questionList.add(new Question("cytryna", "content/17.jpg", 1));
+        questionList.add(new Question("samolot", "content/18.jpg", 1));
+        questionList.add(new Question("papieros", "content/19.jpg", 1));
+        questionList.add(new Question("auto", "content/20.jpg", 2));
+        questionList.add(new Question("chleb", "content/21.jpg", 1));
+        questionList.add(new Question("miasto", "content/22.jpg", 3));
+        questionList.add(new Question("komputer", "content/23.jpg", 1));
+        questionList.add(new Question("kot2", "content/24.jpg", 1));
+        questionList.add(new Question("slonecznik", "content/25.jpg", 2));
+        questionList.add(new Question("plaza", "content/27.jpg", 3));
+        questionList.add(new Question("ulica", "content/28.jpg", 3));
+        questionList.add(new Question("smartfony", "content/29.jpg", 1));
+        questionList.add(new Question("web", "content/30.jpg", 3));
+        questionList.add(new Question("tygrys", "content/31.jpg", 1));
     }
 
     public static void saveDb(String db) {
